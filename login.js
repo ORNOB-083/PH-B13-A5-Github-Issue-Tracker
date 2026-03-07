af4a1d4b7ff8ede638e692de55bccea43c937873
Shalehin Ahmed Ornob <saornob2001@gmail.com>
2026-03-07 23:34:54 +0600

    Login Page Complete

--- login.js
+++ login.js
@@ -1,1 +1,19 @@
-console.log('login page')
+// console.log('login page')
+
+function login() {
+    const username = document.getElementById("username").value
+    const password = document.getElementById("password").value
+
+    if (username === "admin" && password === "admin123") {
+        // alert('Successfully Logged In');
+        window.location.assign('./dashboard.html')
+    }
+    else if (username != "admin") {
+        alert("Invalid username");
+        return;
+    }
+    else if (password != "admin123") {
+        alert("Invalid Password");
+        return;
+    }
+}
