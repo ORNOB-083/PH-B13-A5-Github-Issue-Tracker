f7fe508a3b8c7215cdf90df9e7796935d4a737a0
Shalehin Ahmed Ornob <saornob2001@gmail.com>
2026-03-09 02:38:11 +0600

    Issues box and button click work done

--- dashboard.js
+++ dashboard.js
@@ -1,11 +1,180 @@
-console.log('Dashboard page')
+const cardContainer = document.getElementById("issuesContainer")
+const issueCount = document.getElementById("issueCount")
+const loader = document.getElementById("loader")
+
+let issueStore = []
+
+//Issues
+
+const fetchIssues = () => {
+    showLoader(true)
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+        .then(res => res.json())
+        .then(data => {
+            issueStore = data.data
+            renderIssues(issueStore)
+        })
+}
+
+
+//Show Loader
+
+const showLoader = (state) => {
+
+    if (state) {
+        loader.classList.remove("hidden")
+        cardContainer.classList.add("hidden")
+    }
+    else {
+        loader.classList.add("hidden")
+        cardContainer.classList.remove("hidden")
+    }
+}
+
+
+//Issues
+
+const renderIssues = (issues) => {
+
+    cardContainer.innerHTML = ""
+    issueCount.innerText = issues.length + " Issues"
+
+    issues.forEach(issue => {
+
+        let borderColor = "border-green-500"
+        let statusIcon = "Open-Status.png"
+
+        if (issue.status === "closed") {
+            borderColor = "border-purple-500"
+            statusIcon = "Closed-Status.png"
+        }
+        const card = document.createElement("div");
+        card.className = `bg-white rounded-lg shadow p-5 border-t-4 cursor-pointer ${borderColor}`;
+        card.innerHTML = `
+        <div class="flex justify-between">
+            <img src="./assets/${statusIcon}" class="w-6">
+            <span class="badge">${issue.priority}</span>
+        </div>
+        <h2 class="font-semibold mt-2">${issue.title}</h2>
+        <p class="text-gray-500 text-sm mt-1">${issue.description}</p>
+        <div class="flex flex-wrap gap-2 my-3">
+            ${showLabels(issue.labels)}
+        </div>
+        <hr>
+        <div class="text-gray-500 text-sm mt-3">
+            <p>#${issue.author}</p>
+            <p>#${issue.priority}</p>
+            <p>${issue.createdAt}</p>
+            <p>${issue.updatedAt}</p>
+        </div>
+        `
+        card.addEventListener("click", () => {
+            openIssue(issue.id)
+        })
+        cardContainer.appendChild(card)
+    })
+    showLoader(false);
+}
+
+
+//Label Function
+
+const showLabels = (labels) => {
+
+    const html = labels.map(label => {
+/* bg-[#FDE68A] text-[#D97706] */
+        let color = "badge-neutral";
+
+        if (label === "bug") {
+            color = "badge-error text-red-400 bg-red-200";
+            return `<span class="badge ${color} badge-outline"><i class="fa-solid fa-bug"></i>${label}</span>`
+        }
+        else if (label === "help wanted") {
+            color = "badge-warning text-yellow-600 bg-yellow-200";
+            return `<span class="badge ${color} badge-outline"><i class="fa-solid fa-life-ring"></i>${label}</span>`
+        }
+        else if (label === "enhancement") {
+            color = "badge-success text-green-400 bg-green-100";
+            return `<span class="badge ${color} badge-outline"><i class="fa-solid fa-bolt"></i>${label}</span>`
+        }
+        else if (label === "good first issue") {
+            color = "badge-neutral text-black-400 bg-blue-100";
+            return `<span class="badge ${color} badge-outline"><i class="fa-solid fa-hand-holding-heart"></i>${label}</span>`
+        }
+        else if (label === "documentation") {
+            color = "badge-primary bg-blue-200 text-blue-600";
+            return `<span class="badge ${color} badge-outline"><i class="fa-solid fa-book"></i>${label}</span>`
+        }
+    })
+    return html.join(" ");
+}
+
+
+//Filter Button
+
+document.getElementById("btn-all").addEventListener("click", () => {
+    renderIssues(issueStore);
+})
+
+document.getElementById("btn-open").addEventListener("click", () => {
+    const openList = issueStore.filter(i => i.status === "open")
+    renderIssues(openList);
+})
+
+document.getElementById("btn-closed").addEventListener("click", () => {
+    const closedList = issueStore.filter(i => i.status === "closed")
+    renderIssues(closedList);
+})
+
+
+//Search Button Work
+
+document.getElementById("searchInput").addEventListener("input", () => {
+
+    const text = document.getElementById("searchInput").value.toLowerCase().trim()
+
+    if (text === "") {
+        renderIssues(issueStore);
+        return;
+    }
+
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${text}`)
+        .then(res => res.json())
+        .then(data => {
+            renderIssues(data.data);
+        })
+})
+
+
+//Modal open
+
+const openIssue = (id) => {
+    fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            displayModal(data.data);
+        })
+}
+
+
+//Modal Display
+
+const displayModal = (issue) => {
+    document.getElementById("modal_content").innerHTML = ``;
+}
+
+
+//Button Style
 
 const toggleTab = (id) => {
-    document.querySelectorAll("#btn-all, #btn-open, #btn-closed").forEach(btn => {
+
+    document.querySelectorAll("#btn-all,#btn-open,#btn-closed").forEach(btn => {
         btn.classList.remove("bg-blue-600", "text-white");
         btn.classList.add("bg-white", "text-black/60");
-    });
-    const btn = document.getElementById(id);
-    btn.classList.remove("bg-white", "text-black/60");
-    btn.classList.add("bg-blue-600", "text-white");
-};
+    })
+    const activeBtn = document.getElementById(id);
+    activeBtn.classList.remove("bg-white", "text-black/60");
+    activeBtn.classList.add("bg-blue-600", "text-white");
+}
+
+window.addEventListener("DOMContentLoaded", fetchIssues);
